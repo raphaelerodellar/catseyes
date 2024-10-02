eee747220ee241ac237a8d052fb616d67048d096
Raphaele Rodellar <raphaele@hey.com>
2024-10-02 14:38:39 +0200

    Created a seed for availabilities

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,8 +1,8 @@
 class Availability < ApplicationRecord
   belongs_to :cat
 
-  validates :start_on, :end_on, presence: true, uniqueness: { scope: :cat },
-                                overlap: { scope: :cat_id, exclude_edges: %i[start_on end_on] }
-  validates :end_on, comparison: { greater_than: :start_on }
+  # validates :start_on, :end_on, presence: true, uniqueness: { scope: :cat },
+  #                               overlap: { scope: :cat_id, exclude_edges: %i[start_on end_on] }
+  # validates :end_on, comparison: { greater_than: :start_on }
 
 end
